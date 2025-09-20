8410bdd37f5e4753200e45350dbe4fdbb832e5a4
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-09-20 11:22:58 +0530

    added file server ,

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,5 @@
+use rocket::fs::FileServer;
+
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
@@ -5,4 +7,9 @@ fn rocket() -> _ {
 #[get("/")]
 fn index() -> &'static str {
     "This is the homepage."
+}
+#[post("/analyze")]
+fn analyze() -> &'static str {
+    //will add the AI logic here later
+    "This is the AI response endpoint."
 }
