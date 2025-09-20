321b6e7594a7deefb514ed59ebf165bac005ff94
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-09-20 14:48:40 +0530

    Created Client for APIS

--- src/env.rs
+++ src/env.rs
@@ -33,7 +33,7 @@ pub fn conform_env() {
     let returning_var = path_to_env.is_file();
     if !returning_var {
         println!(
-            "There is no Envirment Variable found , Please Create one .env file with google's api key; Check Github On how to Do it . Exititng the Program"
+            "There is no Envirment Variable found , Please Create one .env file with google's api key; Check ENV_TEMPLATE On how to Do it . Exititng the Program"
         );
         exit(0);
     }
@@ -42,4 +42,8 @@ pub fn conform_env() {
 pub fn get_api_key() -> String {
     let api = env::var("GEMINI_API_KEY").expect("There is No Api for GEMINI, You can Create One");
     return api;
+}
+pub fn get_id() -> String {
+    let id = env::var("GCP_PROJECT_ID").expect("There is No Api for GEMINI, You can Create One");
+    return id;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod env;
 mod server;
+use google_generative_ai_rs::v1beta::VertexAI;
 
 #[macro_use]
 extern crate rocket;
@@ -11,4 +12,11 @@ const LOCALE = "asia-south1";
 fn rocket() -> _ {
     env::conform_env();
     server::setup_server()
+
+}
+fn get_ai_online(){
+    let api_key = env::get_api_key();
+    let project_id = env::get_id();
+    let location = LOCALE;
+    let client = VertexAI::new(&project_id, &location, &api_key);
 }
